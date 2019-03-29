cfde593487159250a2d2cd7858f80983c52f132d
mario <mario@leopardtechs.com>
2019-03-28 23:58:32 -0600

    Updates

---
+++ precache-manifest.f9e37b66fb8665849413b86049c1b2d0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "15df325cb5e24ab7ba93",
+    "url": "/redvoucher/static/css/main.05c4fe25.chunk.css"
+  },
+  {
+    "revision": "15df325cb5e24ab7ba93",
+    "url": "/redvoucher/static/js/main.d71d18fd.chunk.js"
+  },
+  {
+    "revision": "1c0b2b4434af0bfbcb68",
+    "url": "/redvoucher/static/js/runtime~main.386c0dc6.js"
+  },
+  {
+    "revision": "c7838c43b00b23cf0258",
+    "url": "/redvoucher/static/js/2.f4d92516.chunk.js"
+  },
+  {
+    "revision": "3263b93f7cd01f7758fa8925b090df01",
+    "url": "/redvoucher/static/media/promo.3263b93f.png"
+  },
+  {
+    "revision": "a4183e57b9960ca0bb0c66486087dfc2",
+    "url": "/redvoucher/static/media/ticket.a4183e57.png"
+  },
+  {
+    "revision": "8569eccaefc30c1c1c52c7fe13626195",
+    "url": "/redvoucher/static/media/capitana.8569ecca.png"
+  },
+  {
+    "revision": "2f623cc9088b8fee061072979400397d",
+    "url": "/redvoucher/static/media/capitana-logo.2f623cc9.png"
+  },
+  {
+    "revision": "fddf2385bdde97ff55bb428f315e2206",
+    "url": "/redvoucher/static/media/home.fddf2385.jpg"
+  },
+  {
+    "revision": "ed3e0be95dffb1d87786823db0f8ed66",
+    "url": "/redvoucher/index.html"
+  }
+];
